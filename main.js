cb413fba70baa2f8d3252d2a6d26038c66ca8e2d
Rui Gomes <lucasantony815@gmail.com>
2024-03-27 16:23:38 -0300

    aaaaaaaaaaaa

---
+++ main.js
@@ -0,0 +1,89 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+const scrollPercent = () => {
+  const bodyST = document.body.scrollTop;
+  const docST = document.documentElement.scrollTop;
+  const docSH = document.documentElement.scrollHeight;
+  const docCH = document.documentElement.clientHeight;
+  
+  return (docST + bodyST) / (docSH - docCH) * 100
+}
+
+function redirect() {
+  window.location.replace("https://youtu.be/Op9S9rKENrk?si=UoWp2q0KmbOiTwSQ&t=25")
+}
+
+window.onscroll = () => {
+  console.log(scrollPercent())
+};
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 20],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    fontStyle: ["normal"],
+    duration: 2000,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.line',
+    easing: "easeOutExpo",
+    opacity: [1,0],
+    duration: 1000,
+    offset: '-=2000',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11 .letter',
+    easing: "easeOutExpo",
+    fontWeight: [600],
+    duration: 2000,
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11 .letter',
+    easing: "easeOutExpo",
+    fontStyle: ["normal", "italic"],
+    duration: 2000,
+    offset: '-=2000',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11 .letter',
+    easing: "easeOutExpo",
+    fontWeight: [400],
+    duration: 2000,
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11 .letter',
+    easing: "easeOutExpo",
+    fontStyle: ["italic", "normal"],
+    duration: 2000,
+    offset: '-=2000',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11 .letter',
+    easing: "easeOutExpo",
+    fontWeight: [700],
+    duration: 2000,
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
